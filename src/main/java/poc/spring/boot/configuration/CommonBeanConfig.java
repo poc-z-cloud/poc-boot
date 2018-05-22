fccaa9fd71abb0d09f6b53614eec86bfed0ab46e
poc-z-cloud <poc.z.cloud@gmail.com>
2018-05-22 10:59:56 -0400

    added spring-security-with-dao-authentication-provider

---
+++ src/main/java/poc/spring/boot/configuration/CommonBeanConfig.java
@@ -0,0 +1,16 @@
+package poc.spring.boot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonBeanConfig {
+
+//    @Bean
+//    public StrongPasswordEncryptor strongEncryptor(){
+//        StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
+//        return encryptor;
+//    }
+//    
+
+}
---
+++ src/main/java/poc/spring/boot/configuration/SpringSecConfig.java
@@ -0,0 +1,58 @@
+package poc.spring.boot.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SpringSecConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+    @Qualifier("daoAuthenticationProvider")
+    private AuthenticationProvider authenticationProvider;
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder(PasswordEncryptor passwordEncryptor){
+//        PasswordEncoder passwordEncoder = new org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder();
+//        passwordEncoder.setPasswordEncryptor(passwordEncryptor);
+//        return passwordEncoder;
+//    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(UserDetailsService userDetailsService){
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//      in Spring security 5, default encryptor setPasswordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder); 
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return daoAuthenticationProvider;
+    }
+
+    @Autowired
+    public void configureAuthManager(AuthenticationManagerBuilder authenticationManagerBuilder){
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+    }
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+        .authorizeRequests().antMatchers("/","/webjars/**","/css/**","/images/**","/mvc/products","mvc/product/show/*","/console/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin().loginPage("/login").permitAll()
+        .and()
+        .logout().permitAll();
+        
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+
+}
